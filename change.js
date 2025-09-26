0a0ec449c97fb149841f25b85b552d079440862e
Tarun8765 <tarunghavri4@gmail.com>
2025-09-26 11:18:01 +0530

    third commit version 3

--- change.js
+++ change.js
@@ -1,2 +1,5 @@
 console.log("all the changes commit here.");
 console.log("github is configure successfully ");
+
+const sum = (a, b) => a + b / 2;
+console.log(sum(3, 4));
--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 console.log("git and github tutorial ");
 const learnGitAndGitHub = "supersimple dev";
+// learning git become simple with supersimple dev
